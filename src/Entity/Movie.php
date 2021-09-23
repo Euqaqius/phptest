ad96472e316888fd190f94620742abe6c22d68b9
Euqaqius <euqaqius@mail.ru>
2021-09-23 05:25:01 -0700

    Initial symfony installation

---
+++ src/Entity/Movie.php
@@ -0,0 +1,110 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\MovieRepository;
+use DateTime;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=MovieRepository::class)
+ */
+class Movie
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=50, nullable=true)
+     */
+    private ?string $title;
+
+    /**
+     * @ORM\Column(type="string", length=255, nullable=true)
+     */
+    private ?string $link;
+
+    /**
+     * @ORM\Column(type="text", length=255, nullable=true)
+     */
+    private ?string $description;
+
+    /**
+     * @ORM\Column(name="pub_date", type="datetime", nullable=true)
+     */
+    private ?DateTime $pubDate;
+
+    /**
+     * @ORM\Column(type="string", length="255", nullable=true)
+     */
+    private ?string $image;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getTitle(): ?string
+    {
+        return $this->title;
+    }
+
+    public function setTitle(?string $title): self
+    {
+        $this->title = $title;
+
+        return $this;
+    }
+
+    public function getLink(): ?string
+    {
+        return $this->link;
+    }
+
+    public function setLink(?string $link): self
+    {
+        $this->link = $link;
+
+        return $this;
+    }
+
+    public function getDescription(): ?string
+    {
+        return $this->description;
+    }
+
+    public function setDescription(?string $description): self
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+
+    public function getPubDate(): ?DateTime
+    {
+        return $this->pubDate;
+    }
+
+    public function setPubDate(?DateTime $pubDate): self
+    {
+        $this->pubDate = $pubDate;
+
+        return $this;
+    }
+
+    public function getImage(): ?string
+    {
+        return $this->image;
+    }
+
+    public function setImage(?string $image): self
+    {
+        $this->image = $image;
+
+        return $this;
+    }
+}
