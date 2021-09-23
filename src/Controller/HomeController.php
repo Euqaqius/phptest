a709328cefcd7ab3ce2badf39f905f9ae6b228c6
Euqaqius <euqaqius@mail.ru>
2021-09-23 05:44:34 -0700

    Three junior tasks

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,17 +2,21 @@
 
 namespace App\Controller;
 
+use App\Repository\MovieRepository;
+use DateTime;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class HomeController extends AbstractController
 {
-    #[Route('/home', name: 'home')]
     public function index(): Response
     {
+        $className = explode('\\', __CLASS__);
         return $this->render('home/index.html.twig', [
-            'controller_name' => 'HomeController',
+            'currentDate' => (new DateTime())->format('H:i:s d-m-Y'),
+            'className' => end($className),
+            'functionName' => __FUNCTION__,
         ]);
     }
 }
